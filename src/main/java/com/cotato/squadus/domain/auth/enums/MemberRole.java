87bdd03d02c837937bfcb5f25691af0111fb2e14
Dongyoung Kim <145846246+goalSetter09@users.noreply.github.com>
2024-07-13 22:02:53 +0900

    Refactor: MemberRole key, title 형태로 변경

--- src/main/java/com/cotato/squadus/domain/auth/enums/MemberRole.java
+++ src/main/java/com/cotato/squadus/domain/auth/enums/MemberRole.java
@@ -1,6 +1,15 @@
 package com.cotato.squadus.domain.auth.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum MemberRole {
-    MEMBER,
-    CERTIFIED_MEMBER
+
+    MEMBER("ROLE_MEMBER", "일반 회원"),
+    CERTIFIED_MEMBER("ROLE_CERTIFIED_MEMBER", "학교 인증 회원");
+
+    private final String key;
+    private final String title;
 }
