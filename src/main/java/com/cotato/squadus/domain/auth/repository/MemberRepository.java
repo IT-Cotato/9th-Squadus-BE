6d9c6bc9e1e30588c540af14bcc36c623ae1329b
Dongyoung Kim <145846246+goalSetter09@users.noreply.github.com>
2024-07-13 23:13:55 +0900

    Feat: MemberRepository findByMemberId 생성

--- src/main/java/com/cotato/squadus/domain/auth/repository/MemberRepository.java
+++ src/main/java/com/cotato/squadus/domain/auth/repository/MemberRepository.java
@@ -2,10 +2,14 @@ package com.cotato.squadus.domain.auth.repository;
 import com.cotato.squadus.domain.auth.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Boolean existsByUsername(String username);
 
     Member findByUsername(String username);
+
+    Optional<Member> findByMemberId(String memberId);
 
 }
